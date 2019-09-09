9579d1b8a690542986edf729c3528c62e2223bc5
koledanil <koledanil@gmail.com>
2019-09-09 22:17:26 +0300

    финальные исправления

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,3 @@
-// "use strict";
-//
-// var gulp = require("gulp");
-// var plumber = require("gulp-plumber");
-// var sourcemap = require("gulp-sourcemaps");
-// var sass = require("gulp-sass");
-// var postcss = require("gulp-postcss");
-// var autoprefixer = require("autoprefixer");
-// var server = require("browser-sync").create();
-//
-// gulp.task("css", function () {
-//   return gulp.src("source/sass/style.scss")
-//     .pipe(plumber())
-//     .pipe(sourcemap.init())
-//     .pipe(sass())
-//     .pipe(postcss([
-//       autoprefixer()
-//     ]))
-//     .pipe(sourcemap.write("."))
-//     .pipe(gulp.dest("source/css"))
-//     .pipe(server.stream());
-// });
-//
-// gulp.task("server", function () {
-//   server.init({
-//     server: "source/",
-//     notify: false,
-//     open: true,
-//     cors: true,
-//     ui: false
-//   });
-//
-//   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-//   gulp.watch("source/*.html").on("change", server.reload);
-// });
-//
-// gulp.task("start", gulp.series("css", "server"));
-
-
 "use strict";
 
 var gulp = require("gulp");
@@ -139,5 +100,45 @@ gulp.task("server", function () {
   gulp.watch("source/*.html", gulp.series("copyfiles", "f5"));
 });
 
-gulp.task("build", gulp.series("clean", "css", "copyfiles",  "images", "webp"));
+gulp.task("build", gulp.series("clean", "css", "copynormalize", "copyfiles",  "images", "webp"));
 gulp.task("start", gulp.series("build", "server"));
+
+
+
+// "use strict";
+//
+// var gulp = require("gulp");
+// var plumber = require("gulp-plumber");
+// var sourcemap = require("gulp-sourcemaps");
+// var sass = require("gulp-sass");
+// var postcss = require("gulp-postcss");
+// var autoprefixer = require("autoprefixer");
+// var server = require("browser-sync").create();
+//
+// gulp.task("css", function () {
+//   return gulp.src("source/sass/style.scss")
+//     .pipe(plumber())
+//     .pipe(sourcemap.init())
+//     .pipe(sass())
+//     .pipe(postcss([
+//       autoprefixer()
+//     ]))
+//     .pipe(sourcemap.write("."))
+//     .pipe(gulp.dest("source/css"))
+//     .pipe(server.stream());
+// });
+//
+// gulp.task("server", function () {
+//   server.init({
+//     server: "source/",
+//     notify: false,
+//     open: true,
+//     cors: true,
+//     ui: false
+//   });
+//
+//   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+//   gulp.watch("source/*.html").on("change", server.reload);
+// });
+//
+// gulp.task("start", gulp.series("css", "server"));
